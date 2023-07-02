3f2b9dd98ac29950139dc86e3da7ed6a724d73e3
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-02 10:44:10 +0530

    [Chetankoli]-Added Part Time Wages

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,10 +4,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-       // CheckEmployee checkEmployee = new CheckEmployee();
-        //checkEmployee.Main(args);
+        CheckEmployee checkEmployee = new CheckEmployee();
+        checkEmployee.Main(args);
 
         DailyWages dailyWages = new DailyWages();
         dailyWages.Main(args);
+
+        PartTimeWages partTimeWages = new PartTimeWages();
+        partTimeWages.Main(args);
     }
 }
