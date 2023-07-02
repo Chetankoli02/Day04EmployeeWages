5a5d857b1a230eb496428b6eb63a6e89bba75af5
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-02 09:27:17 +0530

    [Chetankoli]-Added Employee Wages

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeWages;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CheckEmployee checkEmployee = new CheckEmployee();
+        checkEmployee.Main(args);
+    }
+}
